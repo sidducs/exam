6b5f2f12cf7169126550c9f91453694ead56a179
sidducs <sidducs2233@gmail.com>
2024-11-12 21:52:41 +0530

    updated

--- Count.js
+++ Count.js
@@ -1,1 +1,22 @@
 
+import React,{useState} from "react";
+function Count(){
+    const [count, setCount] = useState(0);
+    function Increment(){
+        setCount(count+1);
+    }
+    function Decrement(){
+        setCount(count-1);
+    }
+
+  
+    return(
+        <>
+        <h1>Counter:{count}</h1>
+        <button onClick={Increment}>Increment</button>
+        <button onClick={Decrement}>Decrement</button>
+        </>
+    )
+}
+export default Count;
+
